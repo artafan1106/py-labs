8853fdf7470e2d281cd7c67d0289bf189bc7553f
artafan1106 <56221032+artafan1106@users.noreply.github.com>
2021-05-18 23:07:29 +0300

    Add files via upload

---
+++ Lab5.py
@@ -0,0 +1,31 @@
+import random
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+
+a = 1.0
+k = 0.0
+f = 1
+i = []
+
+while f > 0.0001: #проверка на разницу
+    x = random.random()
+    y = random.random()
+    k += (x * x + y * y < 1.0) #расчёт сколько точек попали в окружность
+    f = (math.fabs(math.pi - (4 * k / a))) #считаем разницу
+    print (4 * k / a) #умножение на 4(полный круг) и деление на общее кол-во точек
+    a += 1 #прибавляем к точке
+    i.append(math.fabs(math.pi - (4 * k / a)))
+                              
+                              
+print (math.pi) #ввод pi из библиотеки питона
+
+fig, ax = plt.subplots()
+
+plt.xlabel('Кол-во попыток')
+plt.ylabel('Значение')
+                            
+ax.plot(i, label='Pi')
+ax.legend()
+
+plt.show()
