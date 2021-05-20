b42e2198fd4667ad6318b88214511b23647bc451
artafan1106 <56221032+artafan1106@users.noreply.github.com>
2021-05-20 16:16:55 +0300

    Update Lab5.py

--- Lab5.py
+++ Lab5.py
@@ -15,15 +15,16 @@ while f > 0.0001: #проверка на разницу
     f = (math.fabs(math.pi - (4 * k / a))) #считаем разницу
     print (4 * k / a) #умножение на 4(полный круг) и деление на общее кол-во точек
     a += 1 #прибавляем к точке
-    i.append(math.fabs(math.pi - (4 * k / a)))
+    i.append(math.fabs(4 * k / a))
                               
-                              
+
 print (math.pi) #ввод pi из библиотеки питона
 
 fig, ax = plt.subplots()
 
 plt.xlabel('Кол-во попыток')
 plt.ylabel('Значение')
+plt.title('График зависимости точности расчета значения интеграла в зависимости от числа испытаний')
                             
 ax.plot(i, label='Pi')
 ax.legend()
